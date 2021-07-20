7f2049a90960c887025f6e0f14c90e09d17041c8
KNM4656 <87593480+KNM4656@users.noreply.github.com>
2021-07-20 13:04:16 +0530

    Update AuthClient.java

--- src/main/java/com/cognizant/mfpe/pensionManagementSystem/pensionDisbursement/restClients/AuthClient.java
+++ src/main/java/com/cognizant/mfpe/pensionManagementSystem/pensionDisbursement/restClients/AuthClient.java
@@ -9,7 +9,7 @@ import com.cognizant.mfpe.pensionManagementSystem.pensionDisbursement.model.Auth
 /**
  * To access Authorization Microservice
  */
-@FeignClient(name = "authorization-service", url = "http://authorization-environ.eba-gxumxq57.ap-south-1.elasticbeanstalk.com")
+@FeignClient(name = "authorization-service", url = "http://localhost:9095")
 public interface AuthClient {
 
 	@GetMapping("/validate")
